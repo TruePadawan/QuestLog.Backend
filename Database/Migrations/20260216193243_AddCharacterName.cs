c89280532f62b16f149a8ce436fca70161a6fb0d
Hermes <hermeschigoziri@gmail.com>
2026-02-17 00:05:22 +0100

    AddCharacterName Migration

---
+++ Database/Migrations/20260216193243_AddCharacterName.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestLog.Backend.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCharacterName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CharacterName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CharacterName",
+                schema: "identity",
+                table: "AspNetUsers");
+        }
+    }
+}
