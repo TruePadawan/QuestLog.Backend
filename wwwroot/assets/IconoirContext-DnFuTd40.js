05c63f233068c7bcd111e9efe6e9908cd6482550
Hermes <hermeschigoziri@gmail.com>
2026-03-12 18:04:54 +0100

    remove frontend build

--- wwwroot/assets/IconoirContext-DnFuTd40.js
+++
@@ -1,1 +0,0 @@
-import{R as t}from"./index-BM71IHYH.js";const o=t.createContext({});export{o as t};
--- wwwroot/assets/index-BNQJNmq2.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as a}from"./index-BM71IHYH.js";function o(){return t.jsx(a,{to:"/dashboard/main-quest",replace:!0})}export{o as component};
